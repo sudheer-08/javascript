4ee186945606d71d05ff7b1dbcbc0dfeadec8442
Sudheer Kumar <sudheerk31373@gmail.com>
2026-03-25 08:21:52 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 144552;
+let accountEmail ="sudheer@gmail.com";
+var accountPassword = "1234";
+
+
+accountEmail ="hccd@.com";
+accountPassword ="111";
+
+console.table([accountId, accountEmail, accountPassword]);
